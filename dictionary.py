36a51b22ab7f7a3c00b9525ebdc52d9dc6e6dab3
Niraj Patel <786nirajpatel@gmail.com>
2020-04-17 22:55:14 -0400

    Accounted for bad words
    Put a if else statement so bad words do not break the program.

--- dictionary.py
+++ dictionary.py
@@ -1,1 +1,12 @@
 import json
+
+def translate(word):
+    if word in data:
+        return data[word]
+    else:
+        return "The word doesn't exist. Please double check it."
+
+data = json.load(open("data.json"))
+word = input("Enter word: ")
+
+print(translate(word))

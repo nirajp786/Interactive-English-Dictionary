c509cd39f8bcba68e774a48cb5040d594756daf0
Niraj Patel <786nirajpatel@gmail.com>
2020-04-19 20:29:20 -0400

    Added Best Match
    If user enters an incorrect word it finds a similar word if avaliable

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,13 @@
 import json
+from difflib import get_close_matches
 
 def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
+        return "Did you mean %s instead?" % get_close_matches(word, data.keys(), cutoff=0.8)[0]
+        
     else:
         return "The word doesn't exist. Please double check it."
 

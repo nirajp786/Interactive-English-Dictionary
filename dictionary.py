c7f7b88337ba4e333c784cef8571ff4c17492b7e
Niraj Patel <786nirajpatel@gmail.com>
2020-04-19 21:04:12 -0400

    Completed program
    Added more features for words that are noun. Ex. Delhi is in the dict but delhi is not and same for USA is in the dict but not usa

--- dictionary.py
+++ dictionary.py
@@ -2,11 +2,19 @@ import json
 from difflib import get_close_matches
 
 def translate(word):
-    word = word.lower()
-    if word in data:
+    #word = word.lower()
+    if word.lower() in data or word.upper() in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
     elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
-        return "Did you mean %s instead?" % get_close_matches(word, data.keys(), cutoff=0.8)[0]
+        bestMathch = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys(), cutoff=0.8)[0])
+        if (bestMathch.upper() == "Y"):
+            return data[get_close_matches(word, data.keys(), cutoff=0.8)[0]]
+        elif (bestMathch.upper() == "N"):
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "Not a correct option."
         
     else:
         return "The word doesn't exist. Please double check it."
@@ -14,4 +22,9 @@ def translate(word):
 data = json.load(open("data.json"))
 word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
